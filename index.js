33a9b76b25ade237332a44d3f079e3d0953349fa
Selin Su Varol <selinsuvarol@gmail.com>
2022-05-12 13:37:44 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Write your code here!
+/* Enter the code to remove the node element here */
+document.querySelector("main#main").remove();
+const newHeader = document.createElement("h1");
+newHeader.id = "victory"
+newHeader.innerHTML = "Selin is the champion!";
